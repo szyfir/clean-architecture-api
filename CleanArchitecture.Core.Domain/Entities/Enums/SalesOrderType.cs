ccb4b9f1b259d0390f65780d194d5f005200b0d2
Szymonf <szymon.firlinger@gmail.com>
2021-04-12 21:54:12 +0200

    :sparkles: Add salesorderType do domain SalesOrder model

---
+++ CleanArchitecture.Core.Domain/Entities/Enums/SalesOrderType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Core.Domain.Entities.Enums
+{
+    public enum SalesOrderType
+    {
+        Normal,
+        Extended
+    }
+}
--- CleanArchitecture.Core.Domain/Entities/SalesOrder.cs
+++ CleanArchitecture.Core.Domain/Entities/SalesOrder.cs
@@ -1,4 +1,5 @@
 ﻿using CleanArchitecture.Core.Domain.Common;
+using CleanArchitecture.Core.Domain.Entities.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +8,10 @@ namespace CleanArchitecture.Core.Domain.Entities
 {
     public class SalesOrder : AuditableEntity
     {
-        public Guid Id { get; }
-        public string Name { get; }
-        public bool IsDepracted { get; }
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public bool IsDepracted { get; private set; }
+        public SalesOrderType SalesOrderType { get; private set; }
 
         private SalesOrder()
         {
@@ -20,8 +22,14 @@ namespace CleanArchitecture.Core.Domain.Entities
             Id = Guid.NewGuid();
             Name = name;
             IsDepracted = false;
+            SalesOrderType = SalesOrderType.Normal;
         }
 
         public static SalesOrder Create(string name) => new SalesOrder(name);
+
+        public void SetType(SalesOrderType salesOrderType)
+        {
+            SalesOrderType = salesOrderType;
+        }
     }
 }
